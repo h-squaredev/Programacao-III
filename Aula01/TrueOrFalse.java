b8d1fd5a31241f8ea6622d3218cf3eceb50250f4
Humberto Henrique <humbertohenrique04@gmail.com>
2026-08-24 00:09:33 -0300

    feat: initial version

---
+++ Aula01/TrueOrFalse.java
@@ -0,0 +1,22 @@
+public class TrueOrFalse {
+        static boolean var1 = true;
+        static boolean var2 = false;
+        static boolean var3 = true;
+        public static void main(String[] args) {
+            if (var1) {
+                System.out.println("A var1 é verdadeira!");
+            }else{
+                System.out.println("A var1 é falsa!");
+            }
+            if (var2) {
+                System.out.println("A var2 é verdadeira!");
+            }else{
+                System.out.println("A var2 é falsa!");
+            }
+            if (var3) {
+                System.out.println("A var3 é verdadeira!");
+            }else{
+                System.out.println("A var3 é falsa!");
+            }
+        }
+    }
