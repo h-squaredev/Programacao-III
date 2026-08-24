49869f3c0fa9f64ffa1574827e21dab305ceab99
Humberto Henrique <humbertohenrique04@gmail.com>
2026-08-24 00:21:33 -0300

    refactor: apply clean code practices and reduce code duplication

--- Aula01/TrueOrFalse.java
+++ Aula01/TrueOrFalse.java
@@ -1,22 +1,15 @@
 public class TrueOrFalse {
-        static boolean var1 = true;
-        static boolean var2 = false;
-        static boolean var3 = true;
-        public static void main(String[] args) {
-            if (var1) {
-                System.out.println("A var1 é verdadeira!");
+    public static void main(String[] args) {
+        // As variaveis agora sao locais e as armazenei em um vetor
+        boolean[] listaBooleana = {true, false, true};
+
+        // Implementacao de laco para nao repetir if
+        for (int i = 0; i < listaBooleana.length; i++){
+            if (listaBooleana[i]) {
+                System.out.println("A variavel " + (i + 1) + " eh verdadeira!");
             }else{
-                System.out.println("A var1 é falsa!");
+                System.out.println("A variavel " + (i + 1) + " eh falsa!");
             }
-            if (var2) {
-                System.out.println("A var2 é verdadeira!");
-            }else{
-                System.out.println("A var2 é falsa!");
-            }
-            if (var3) {
-                System.out.println("A var3 é verdadeira!");
-            }else{
-                System.out.println("A var3 é falsa!");
-            }
-        }
+        } 
     }
+}
