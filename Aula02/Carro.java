2b10500ecbf837fed7715be016a82bc0f611d4a6
Humberto Henrique <humbertohenrique04@gmail.com>
2026-09-07 10:57:48 -0300

    feat: first task of Aula02 completed

--- Aula02/Carro.java
+++ Aula02/Carro.java
@@ -1,5 +1,31 @@
 package Aula02;
 
+// Classe principal
 public class Carro {
+    // Atributos
+    String marca;
+    String modelo;
+    int ano;
     
+    // Metodos sem parametros
+    void exibirInfo(){
+        System.out.println("Seu Carro: " + marca + ", " + modelo + ", " + ano);
+    }
+
+    // Main
+    public static void main(String[] args) {
+        //Primeira instancia
+        Carro carro1 = new Carro();
+        carro1.marca = "Volkswagen";
+        carro1.modelo = "Voyage";
+        carro1.ano = 2010;
+        carro1.exibirInfo();
+        
+        //Segunda instancia
+        Carro carro2 = new Carro();
+        carro2.marca = "Chevrolet";
+        carro2.modelo = "Onix";
+        carro2.ano = 2022;
+        carro2.exibirInfo();
+    }
 }
